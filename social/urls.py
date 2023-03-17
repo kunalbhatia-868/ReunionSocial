c80c4178591b0a5a4f32984d789b0cb5e886bd23
kunalbhatia-868 <kunalbhatia868@gmail.com>
2023-03-17 16:57:37 +0530

    urls updated

--- social/urls.py
+++ social/urls.py
@@ -8,14 +8,14 @@ LikeView,
 UserDetail,AuthenticateView,Follow,UnFollow
 )
 urlpatterns = [
-    path("authenticate/",AuthenticateView.as_view(),name="authenticate"),
-    path('user/',UserDetail.as_view(),name="user_detail"),
+    path("authenticate",AuthenticateView.as_view(),name="authenticate"),
+    path('user',UserDetail.as_view(),name="user_detail"),
     path('follow/<uuid:user_id>',Follow.as_view(),name="follow"),
     path('unfollow/<uuid:user_id>',UnFollow.as_view(),name="unfollow"),
-    path("posts/<uuid:post_id>/",PostDetailDeleteView.as_view(),name="post_detail_delete"),
-    path('posts/',PostCreateView.as_view(),name="create_post"),
-    path('all_posts/',AllPostView.as_view(),name='all_posts'),
-    path('comment/<uuid:post_id>/',CommentCreateView.as_view(),name="create_comment"),
-    path('like/<uuid:post_id>/',LikeView.as_view(),name="like"),
-    path('unlike/<uuid:post_id>/',UnLikeView.as_view(),name="unlike"),   
+    path("posts/<uuid:post_id>",PostDetailDeleteView.as_view(),name="post_detail_delete"),
+    path('posts',PostCreateView.as_view(),name="create_post"),
+    path('all_posts',AllPostView.as_view(),name='all_posts'),
+    path('comment/<uuid:post_id>',CommentCreateView.as_view(),name="create_comment"),
+    path('like/<uuid:post_id>',LikeView.as_view(),name="like"),
+    path('unlike/<uuid:post_id>',UnLikeView.as_view(),name="unlike"),   
 ]
